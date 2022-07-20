d7346975091af8bb4062132bdb9b5b9306dc2bd6
AndreaCardnas <109403169+AndreaCardnas@users.noreply.github.com>
2022-07-19 21:07:08 -0500

    feature: format main.js and adding files for format (#1)
    Co-authored-by: Andrea Cardenas <jazmin.cardenasg@gmail.com>

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 // Get modal element
-const modal = document.getElementById('simpleModal')
+const modal = document.getElementById('simpleModal');
 // Get open modal button
-const modalBtn = document.getElementById('modalBtn')
+const modalBtn = document.getElementById('modalBtn');
 // Get close button
-const closeBtn = document.getElementsByClassName('closeBtn')[0]
+const closeBtn = document.getElementsByClassName('closeBtn')[0];
 
 // Listen for open7 click
-modalBtn.addEventListener('click', openModal)
+modalBtn.addEventListener('click', openModal);
 // Listen for close click
-closeBtn.addEventListener('click', closeModal)
+closeBtn.addEventListener('click', closeModal);
 // Listen for outside click
-window.addEventListener('click', outsideClick)
+window.addEventListener('click', outsideClick);
 
 // Function to open modal
-function openModal () {
-  modal.style.display = 'block'
-};
+function openModal() {
+  modal.style.display = 'block';
+}
 
 // Function to close modal
-function closeModal () {
-  modal.style.display = 'none'
-};
+function closeModal() {
+  modal.style.display = 'none';
+}
 
 // Function to close modal if outside click
-function outsideClick (e) {
+function outsideClick(e) {
   if (e.target === modal) {
-    modal.style.display = 'none'
+    modal.style.display = 'none';
   }
-};
+}
